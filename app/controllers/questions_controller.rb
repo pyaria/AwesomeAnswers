c0fb84243bd1d6d433d254b3d6e200a7a0407ff5
PYARIA <jadetong@gmail.com>
2015-10-30 10:08:34 -0700

    refactored CRUD

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,11 @@
 # generate a controller by running:
 # $rails g controller questions
 class QuestionsController < ApplicationController
+  # before action will register a method (in thie scase call find_question)
+  # that will be executed before all actions unless you specify options such as:
+  # :except or :only (SO A SUBSET OF ACTIONS)
+  # before_action is a method that comes with RAILS (convention)
+  before_action(:find_question, {only: [:show, :edit, :update, :destroy]})
   def new
     # the default behavior of controller action is to render a template
     # within a folder with the same controller name using the format/templating language used
@@ -15,12 +20,11 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    # Question.create({title: params[:question[:title]], body: params[:question[:body]]})
-    question_params = params.require(:question).permit([:title, :body])
+
     @q = Question.new(question_params)
     if @q.save
       # render text: "Record saved!"
-      redirect_to(question_path(@q))
+      redirect_to(question_path(@q), notice: "Question created!")
     else
       # render text: "Record not saved!"
       # render text: "Didn't save correctly! #{q.errors.full_messages.join(", ")}"
@@ -34,21 +38,16 @@ class QuestionsController < ApplicationController
   # you get access to the id in the url from params[:id]
   def show
     # finding the question by its id
-    @q = Question.find(params[:id])
     # default: render: views/questions/show.html.erb
   end
 
   def edit
-    @q = Question.find(params[:id])
-    @q = Question.find(params[:id])
   end
 
   def update
-    @q = Question.find(params[:id])
-    question_params = params.require(:question).permit([:title, :body])
     @q.update(question_params)
     if @q.save
-      redirect_to(question_path(@q))
+      redirect_to(question_path(@q), :notice "Question updated!")
     else
       render :edit
     end
@@ -59,8 +58,21 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @q = Question.find(params[:id])
     @q.destroy
-    redirect_to questions_path
+    # flash[:notice] = "Question deleted successfully"
+    redirect_to(questions_path, notice: "Questions deleted successfully")
   end
+
+  private
+  def find_question
+    # finding the question by its id
+    # default: render: views/questions/show.html.erb
+    @q = Question.find(params[:id])
+  end
+
+  def question_params
+    # Question.create({title: params[:question[:title]], body: params[:question[:body]]})
+    params.require(:question).permit([:title, :body])
+  end
+
 end
